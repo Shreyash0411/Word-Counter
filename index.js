078bb8b0f42693aeddd2c2c23a8a1fb5a5685edc
Shreyash0411 <93145314+Shreyash0411@users.noreply.github.com>
2023-02-18 16:33:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let textBox = document.getElementById("textBox");
+
+textBox.addEventListener('input', function(){
+    var text = this.value;
+    let char = text.length;
+
+    document.getElementById("char").innerHTML = char;
+
+    text = text.trim();
+    let words = text.split(" ");
+    let cleanList = words.filter(function(elm){
+        return elm != "";
+    });
+    console.log(cleanList);
+     
+    document.getElementById("word").innerHTML = cleanList.length;
+})
